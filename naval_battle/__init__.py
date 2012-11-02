d263edb35026b889f8cafce3d4af897187afa7fb
Mak Tomilov <mak.tomilov@gmail.com>
2012-11-02 15:33:50 +0600

    config web-app and database

---
+++ naval_battle/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_DB"] = "naval_battle"
+# app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+if __name__ == '__main__':
+    app.run()
+
